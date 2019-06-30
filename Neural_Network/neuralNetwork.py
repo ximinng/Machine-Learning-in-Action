35cd85afc346c70b1cbae42334d40d768899096f
xxm <ximingxing@gmail.com>
2019-06-30 23:53:19 +0800

    neunal network __init__ and query function done

--- Neural_Network/neuralNetwork.py
+++ Neural_Network/neuralNetwork.py
@@ -1,17 +1,67 @@
 # -*- coding: utf-8 -*-
 """
-   Description :
+   Description : neural network class definition
    Author :        xxm
 """
+import numpy as np
+import scipy.special as func
 
 
 class neuralNetwork:
 
-    def __init__(self):
+    def __init__(self,
+                 input_nodes,
+                 hidden_nodes,
+                 output_nodes,
+                 learning_rate):
+        """initialise the neural network"""
+
+        self.inodes = input_nodes
+        self.hnodes = hidden_nodes
+        self.onodes = output_nodes
+
+        self.lr = learning_rate
+
+        # link weight matrices, wih(weight input2hidden) and who(weight hidden2output)
+        self.wih = np.random.normal(loc=0.0,
+                                    scale=pow(self.hnodes, -0.5),
+                                    size=(self.hnodes, self.inodes))
+        self.who = np.random.normal(loc=0.0,
+                                    scale=pow(self.hnodes, -0.5),
+                                    size=(self.onodes, self.hnodes))
+
+        # activation function is the sigmoid
+        self.activation_function = lambda x: func.expit(x)
+
+    def train(self):
         pass
 
-    def train():
-        pass
+    def query(self,
+              inputs_list):
+        """query the neural network"""
+        # convert inputs list to 2d list
+        inputs = np.array(inputs_list, ndmin=2).T
 
-    def query():
-        pass
+        # calculate the signals into hidden layer
+        hidden_inputs = np.dot(self.wih, inputs)
+
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = np.dot(self.who, hidden_outputs)
+
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
+
+
+if __name__ == '__main__':
+    input_nodes = 3
+    hidden_nodes = 3
+    output_nodes = 3
+
+    learning_rate = 0.5
+
+    n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
