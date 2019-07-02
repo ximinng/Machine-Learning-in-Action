305c728fe3d0068e617a1ee4535bca0bab69c503
xxm <ximingxing@gmail.com>
2019-07-02 23:30:58 +0800

    neunal network train method done

--- Neural_Network/neuralNetwork.py
+++ Neural_Network/neuralNetwork.py
@@ -33,7 +33,37 @@ class neuralNetwork:
         # activation function is the sigmoid
         self.activation_function = lambda x: func.expit(x)
 
-    def train(self):
+    def train(self,
+              inputs_list,
+              targets_list):
+        """train the neural network"""
+        """query the neural network"""
+        # convert inputs list to 2d list
+        inputs = np.array(inputs_list, ndmin=2).T
+        targets = np.array(targets_list, ndmin=2).T
+
+        # calculate the signals into hidden layer
+        hidden_inputs = np.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = np.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        # error
+        output_errors = targets - final_outputs
+        # hidden layer error is the output_errors,split by weights,recombined at hidden nodes
+        hiddne_errors = np.dot(self.who.T, output_errors)
+
+        # update the weights for the links between the hidden and output layers
+        self.who += self.lr * np.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                     np.transpose(hidden_outputs))
+
+        # update the weights for the links between the hidden and output layers
+        self.wih += self.lr * np.dot((hiddne_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                     np.transpose(inputs))
         pass
 
     def query(self,
@@ -65,3 +95,5 @@ if __name__ == '__main__':
     learning_rate = 0.5
 
     n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+    print(n.query([1.0, 0.5, -1.5]))
